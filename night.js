5296d422681905cd0ad721d6ba3a49a6196c482b
danielsanchezcol7 <183245501+danielsanchezcol7@users.noreply.github.com>
2024-10-06 23:12:17 -0500

    nuevos modificados

---
+++ night.js
@@ -0,0 +1,12 @@
+function modoOscuro(){
+    document.getElementById("body").classList.add("oscuro")
+    document.getElementById("nav").classList.add("dark")
+    document.getElementById("footer").classList.add("dark")
+    document.getElementsByTagName()
+}
+
+let cookies="Nuestro sitios usa click, al dar click aceptas nuestros términos de servicio"
+alert(cookies);
+
+let tratamientoInfo="La informacion recopilada no será para terceros Aveas Data"
+alert(tratamientoInfo):
---
+++ scripts.js
@@ -0,0 +1,34 @@
+/* clase declaracion variables
+var ejemplo; // variable tipo global
+
+let variableConLet; //camel Case caracteres, variable local
+
+const variableConConst()
+*/
+
+let saludo ="Hola Mundo";
+console.log(saludo);
+alert(saludo);
+
+
+/*Tipos de datos:
+numericos:
+enteros = integer: 2,4,5, -4
+decimales = float 1,2; 2 3,0,5
+binarios = binary ej
+
+2 texto:
+Cadenas de texto
+3 Boolenanos:
+Falso o verdaderos
+
+4: Bloob tiny; big
+biometria en boob, imagenes, almacenar musica etc..
+
+*/ 
+
+
+let nombre = prompt("Digita tu nombre");
+let apellido = prompt("Digita tu apellido");
+
+alert("El usuario se llama: " + nombre + " " + apellido);
